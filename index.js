5e0d40dcbcfb79b5ac687d2668c03aa4ea03bcff
Nate Baker <nathansbaker@gmail.com>
2022-10-25 23:24:24 -0600

    Connect to remote db

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ app.use(cors());
 const knex = require('knex')({
   client: 'mysql2',
   connection: {
-    host : '127.0.0.1',
+    host : 'classproject.cluster-cbdo9oytyewz.us-east-2.rds.amazonaws.com',
     port : 3306,
-    user : 'root',
-    password : 'Password1!',
+    user : 'admin',
+    password : 'admin123',
     database : 'studentemploymentdb'
   }
 });
