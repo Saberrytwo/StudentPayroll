418cc8fb81cb9f5736dac7b3934c7357ba1f87ac
Nate Baker <nathansbaker@gmail.com>
2022-10-26 13:58:28 -0600

    This is a funny one

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require("body-parser");
 const app = express()
 const port = 3001
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const knex = require('knex')({
   client: 'mysql2',
@@ -41,9 +44,15 @@ app.get('/get-table-data', async (req, res) => {
     })
   }));
 
-  console.log(supervisorsWithEmployees);
   res.send(supervisorsWithEmployees);
 })
+
+app.post('/update-row', (req, res) => {
+  knex('Employee').where('byuId', req.body.byuId)
+  .update({
+    [req.body.column]: req.body.value
+  }).then(result => { res.status(200); res.send(); })
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
