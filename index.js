7975afa84a2afc1b64c64f9aa3ccb0d421572259
Saberrytwo <90337853+Saberrytwo@users.noreply.github.com>
2022-11-29 19:18:11 -0700

    testing something

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require("body-parser");
 const app = express()
-const port = 3001
+const port = 3000
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
